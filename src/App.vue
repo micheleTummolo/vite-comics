12d9c90adf50af2e3aafbe3a4d83f5ceaa8ba578
micheleTummolo <tummolomichele02@gmail.com>
2023-01-11 19:55:44 +0100

    Done header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+
 export default {
+  components: {
+    AppHeader,
+  }
 
 }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader></AppHeader>
   </div>
 </template>
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,138 @@
 <script>
 export default {
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'characters',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'comics',
+                    url: '#',
+                    active: true
+                },
+                {
+                    label: 'movies',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'tv',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'games',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'collectibles',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'videos',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'fans',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'news',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'shop',
+                    url: '#',
+                    active: false
+                },
+            ]
+        }
+    }
 
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
-</template>
-<style lang="">
+    <header>
+        <div class="header_container">
+            <div class="logo_container">
+                <img src="../../src/assets/images/dc-logo.png" alt="">
+            </div>
     
+            <nav>
+                <ul>
+                    <li v-for="(item, index) in menu">
+                        <a :href="item.url" :class="item.active ? 'active' : ''">{{item.label}}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+header {
+    background-color: $color_white;
+
+
+    .header_container {
+        width: 1000px;
+        margin: 0 auto;
+        @include flex_between;
+
+        .logo_container {
+            @include flex_align_center;
+
+            img {
+                width: 50px;
+                height: 50px;
+            }
+        }
+
+        nav {
+            @include flex_align_center;
+            padding-top: 1.5rem;
+
+            ul {
+
+                display: flex;
+
+                li {
+                    text-transform: uppercase;
+                    margin-right: 1.7rem;
+
+                    a {
+                        color: black;
+                        padding-bottom: 2rem;
+                        display: block;
+                        font-size: .7rem;
+                        font-weight: 500;
+
+
+                        &:hover {
+                            color: $primary_color;
+                        }
+                    }
+
+                    .active {
+                        color: $primary_color;
+                        border-bottom: 4px solid rgb(2, 130, 249);
+                    }
+                }
+            }
+
+        }
+    }
+
+
+}
 </style>
