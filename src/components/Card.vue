6f0b052a759768a596d8b92c2bfb36e2feacf405
micheleTummolo <tummolomichele02@gmail.com>
2023-01-12 17:55:38 +0100

    Done cards section

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+
+    props: {
+        img: String,
+        title: String
+    }
+
+}
+</script>
+<template>
+    <div class="card">
+        <div class="thumb">
+            <img :src="img" alt="img">
+        </div>
+        <h4>{{ title }}</h4>
+    </div>
+</template>
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.card {
+
+    width: calc((80vw / 6) - 1rem);
+    margin: 0 .5rem 2rem .5rem;
+
+    .thumb {
+
+        img {
+            object-fit: cover;
+            width: 100%;
+            height: calc((80vw / 6) - 1rem);
+            object-position: top;
+            display: block;
+        }
+    }
+
+    h4 {
+        margin-top: 1rem;
+        text-transform: uppercase;
+        color: $color_white;
+        font-size: .8rem;
+    }
+}
+</style>
